d835106458f9fff46f032339257fa7bce03db8de
Rudivan Paixão Barbosa <51829044+rudivanbarbosa@users.noreply.github.com>
2022-03-16 22:06:39 -0300

    Update and rename gdc (Consecutive integer checking algorithm).cpp to gcd (Consecutive integer checking algorithm).cpp

--- gcd (Consecutive integer checking algorithm).cpp
+++ gcd (Consecutive integer checking algorithm).cpp
@@ -11,12 +11,12 @@ else{return a;}
 }
 
 
-int gdc(int m, int n){
+int gcd(int m, int n){
 
 int t = minimo(m,n);
 for(int i = t; i<=t; i--){
     if(m%t == 0 & n%t==0){
-        printf("O máximo divisor comum entre %d e %d é %d.\n ", m, n, t);
+        printf("O mÃ¡ximo divisor comum entre %d e %d Ã© %d.\n ", m, n, t);
         break;
     }else{t = t-1; }
 
@@ -26,7 +26,7 @@ for(int i = t; i<=t; i--){
 int main(){
 setlocale(LC_ALL,"Portuguese");
 
-gdc(31415,14142);
+gcd(31415,14142);
 
 
 }
